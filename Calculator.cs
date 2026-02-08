1dcc4f85bde298e192e088b636931f24ed70dc16
Rey <reipiol42@gmail.com>
2026-02-08 23:40:05 +0800

    Update Calculator.cs para tarong

--- Calculator.cs
+++ Calculator.cs
@@ -2,11 +2,24 @@ using System;
 
 public static class Calculator
 {
-    public static double? Calculate(int first, int second, string operation)
+    public static void Main(string[] args)
     {
-        //write your business logic in here
-
-        return null;
+    int num1, num2;
+    char operation = ' ';
+    
+    Console.Write("Enter first number: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("Enter second number: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    
+    double? result = Calculate(num1, num2, operation);
+    
+    if (result == null)
+            Console.WriteLine("Result: null");
+    else
+            Console.WriteLine("Result: " + result);
+    
     }
 }
 
